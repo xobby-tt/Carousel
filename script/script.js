629f17681cc4e0ba0c44877531895a43878921da
xobby_tt <nastya-nastenkaa@mail.ru>
2017-11-20 20:25:58 +0300

    speed button

--- script/script.js
+++ script/script.js
@@ -4,24 +4,50 @@ var imgWidth = imgRect.width;
 var displayAmount = 3;
 
 carousel.style.width = imgWidth * displayAmount + "px";
+console.log(carousel.style.width)
+
+var line = document.createElement("div");
+line.classList.add("line");
+line.style.marginLeft = parseInt(carousel.style.width)/2 + "px"; 
+document.body.appendChild(line); //carousel.appendChild(line)
 
 var carouselElements = document.querySelectorAll('#carousel > ul > li');
 
+var speedButton = document.createElement("div");
+speedButton.classList.add("speed-button");
+speedButton.innerHTML = "Click";
+document.body.appendChild(speedButton);
 
-var timerId = setInterval(spin, 2000);
-  
-  // через 30 сек остановить повторы
-setTimeout(function() { 
-    clearInterval(timerId);
-}, 30000);
+var startTime;
+var endTime;
 
-function spin() {         
-        //foo();
-        //carouselUl.style.marginLeft = 0 +"px";
-        carouselUl.appendChild(carouselUl.firstElementChild);
-
+speedButton.onmousedown = function() {
+    startTime = new Date;    
 }
 
+speedButton.onmouseup = function() {
+    endTime = new Date;
+    speedSpin(startTime, endTime);    
+}
+
+function speedSpin(start, end) {
+    var durity = end - start;
+    var begin = new Date;
+    var timer = setInterval(function() {
+        var timePassed = Date.now() - begin;    
+        
+            if (timePassed >= durity*5) {
+                clearInterval(timer); 
+                return;
+              }
+        
+            carouselUl.appendChild(carouselUl.firstElementChild);
+    }, 50);
+    
+    
+}
+
+//for beautify scrolling (to do)
 function foo()  {
     for(var j = 0; j < imgRect.width; j+=0.01) 
         carouselUl.style.marginLeft = -j + "px";
